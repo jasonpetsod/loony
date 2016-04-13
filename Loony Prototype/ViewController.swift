0b77ba100b1ef1c46ecdde51ef5136f4cebe0ef6
Jason Petsod <jason@petsod.org>
2016-04-12 20:20:40 -0400

    Initial Commit

---
+++ Loony Prototype/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Loony Prototype
+//
+//  Created by Jason Petsod on 4/12/16.
+//  Copyright © 2016 Jason Petsod. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
