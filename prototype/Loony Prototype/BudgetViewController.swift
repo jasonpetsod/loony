1f10e8b084b775feefc915a135b973088be1da82
Jason Petsod <jason@petsod.org>
2016-05-07 10:01:04 -0400

    Clean up vestigial outlets and actions from BudgetViewController

--- prototype/Loony Prototype/BudgetViewController.swift
+++ prototype/Loony Prototype/BudgetViewController.swift
@@ -3,8 +3,8 @@ import Cocoa
 class BudgetViewController: NSViewController {
   let model: SQLiteModel
 
-  @IBOutlet weak var nameField: NSTextField!
-  
+  // MARK: Initializers
+
   required init?(coder: NSCoder) {
     do {
       model = try SQLiteModel()
@@ -13,17 +13,5 @@ class BudgetViewController: NSViewController {
     }
 
     super.init(coder: coder)
-  }
-  
-  @IBAction func addCategoryClicked(sender: AnyObject) {
-    let name = nameField.stringValue
-    let category = Category.new(name,
-                                parentId: nil,
-                                notes: nil)
-
-    // TODO: handle exception.
-    try! model.addCategory(category)
-
-    print("Added category \(name) with ID \(category.id)")
   }
 }
