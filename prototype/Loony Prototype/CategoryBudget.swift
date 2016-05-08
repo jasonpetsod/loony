0debd8ec2a9f57740c7aaed09f056a43b9aeb4b5
Jason Petsod <jason@petsod.org>
2016-05-08 14:53:47 -0400

    Remove CategoryBudget.category field
    CategoryBudget instances are already linked to Category instances in the
    Category's "budgets" field. There's no need for a reverse link (yet).

--- prototype/Loony Prototype/CategoryBudget.swift
+++ prototype/Loony Prototype/CategoryBudget.swift
@@ -2,15 +2,13 @@ import Foundation
 
 class CategoryBudget {
   let month: Month
-  let category: Category
   let budgetCents: Int
   let carryOverOverspending: Bool
   let notes: String
 
-  init(month: Month, category: Category, budgetCents: Int,
-       carryOverOverspending: Bool, notes: String) {
+  init(month: Month, budgetCents: Int, carryOverOverspending: Bool,
+       notes: String) {
     self.month = month
-    self.category = category
     self.budgetCents = budgetCents
     self.carryOverOverspending = carryOverOverspending
     self.notes = notes
