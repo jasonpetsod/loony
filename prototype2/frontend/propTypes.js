111ce0c5401a126650a6dc70ac48bbed22a655ba
Jason Petsod <jason@petsod.org>
2017-02-04 11:13:44 -0500

    Make propTypes.js lint clean

--- prototype2/frontend/propTypes.js
+++ prototype2/frontend/propTypes.js
@@ -1,15 +1,18 @@
 import React from 'react';
 
 const transaction = React.PropTypes.shape({
-    id: React.PropTypes.string.isRequired,
-    // Milliseconds since the epoch.
-    dateMs: React.PropTypes.number.isRequired,
-    account: React.PropTypes.string.isRequired,
-    payee: React.PropTypes.string.isRequired,
-    category: React.PropTypes.string,
-    memo: React.PropTypes.string,
-    outflow: React.PropTypes.number,
-    inflow: React.PropTypes.number,
+  id: React.PropTypes.string.isRequired,
+  // Milliseconds since the epoch.
+  dateMs: React.PropTypes.number.isRequired,
+  account: React.PropTypes.string.isRequired,
+  payee: React.PropTypes.string.isRequired,
+  category: React.PropTypes.string,
+  memo: React.PropTypes.string,
+  outflow: React.PropTypes.number,
+  inflow: React.PropTypes.number,
 });
 
-export {transaction};
+/* eslint-disable import/prefer-default-export */
+// TODO: Remove this lint suppression once we add more propTypes to this file.
+export { transaction };
+/* eslint-enable import/prefer-default-export */
