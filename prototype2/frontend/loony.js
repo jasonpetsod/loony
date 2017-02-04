fc3023a80820cea73714b1eee3783dace7ac8830
Jason Petsod <jason@petsod.org>
2017-02-04 01:07:43 -0500

    Add the ability to add new transactions

--- prototype2/frontend/loony.js
+++ prototype2/frontend/loony.js
@@ -2,6 +2,7 @@ import accounting from 'accounting';
 import moment from 'moment';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import uuid from 'uuid';
 
 const transactionPropType = React.PropTypes.shape({
     id: React.PropTypes.string.isRequired,
@@ -39,9 +40,129 @@ class TransactionRow extends React.Component {
   }
 }
 
+class AddTransactionRow extends React.Component {
+  static propTypes = {
+    // TODO: Add function signature.
+    newTransactionHander: React.PropTypes.func
+  }
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      'account': '',
+      'date': '',
+      'payee': '',
+      'category': '',
+      'memo': '',
+      'outflow': '',
+      'inflow': ''
+    };
+  }
+
+  handleInputChange = (event) => {
+    const target = event.target;
+    const name = target.name;
+    const value = target.value;
+    this.setState({
+      [name]: value
+    })
+  }
+
+  handleAdd = (event) => {
+    const transaction = {
+      id: uuid.v4(),
+      dateMs: moment(this.state.date).valueOf(),
+      account: this.state.account,
+      payee: this.state.payee,
+      category: this.state.category,
+      memo: this.state.memo,
+      // TODO: Using floats for currencies is so, so wrong.
+      outflow: parseFloat(this.state.outflow),
+      inflow: parseFloat(this.state.inflow),
+    }
+    this.props.newTransactionHandler(transaction);
+  }
+
+  render() {
+    return (
+        <tr>
+          <td>
+            <input
+              type="text"
+              name="account"
+              placeholder="Account"
+              value={this.state.account}
+              onChange={this.handleInputChange}
+            />
+          </td>
+          <td>
+            <input
+              type="date"
+              name="date"
+              placeholder="Date"
+              value={this.state.date}
+              onChange={this.handleInputChange}
+            />
+          </td>
+          <td>
+            <input
+              type="text"
+              name="payee"
+              placeholder="Payee"
+              value={this.state.payee}
+              onChange={this.handleInputChange}
+            />
+          </td>
+          <td>
+            <input
+              type="text"
+              name="category"
+              placeholder="Category"
+              value={this.state.category}
+              onChange={this.handleInputChange}
+            />
+          </td>
+          <td>
+            <input
+              type="text"
+              name="memo"
+              placeholder="Memo"
+              value={this.state.memo}
+              onChange={this.handleInputChange}
+            />
+          </td>
+          <td>
+            <input
+              type="text"
+              name="outflow"
+              placeholder="Outflow"
+              value={this.state.outflow}
+              onChange={this.handleInputChange}
+            />
+          </td>
+          <td>
+            <input
+              type="text"
+              name="inflow"
+              placeholder="Inflow"
+              value={this.state.inflow}
+              onChange={this.handleInputChange}
+            />
+          </td>
+          <td>
+            <input type="submit" value="Add" onClick={this.handleAdd} />
+          </td>
+        </tr>
+    );
+  }
+}
+
 class TransactionTable extends React.Component {
   static propTypes = {
     transactions: React.PropTypes.arrayOf(transactionPropType),
+    // TODO: Add function signature.
+    newTransactionHandler: React.PropTypes.func
   };
 
   render() {
@@ -63,6 +184,8 @@ class TransactionTable extends React.Component {
         </thead>
         <tbody>
         {rows}
+        <AddTransactionRow
+          newTransactionHandler={this.props.newTransactionHandler} />
         </tbody>
         </table>
     );
@@ -77,11 +200,19 @@ class App extends React.Component {
     }
   }
 
+  addTransaction = (transaction) => {
+    let transactions = this.state.transactions;
+    transactions.push(transaction);
+    this.setState({transactions: transactions});
+  }
+
   render() {
     return (
         <div>
         <h1>Loony</h1>
-        <TransactionTable transactions={this.state.transactions} />
+        <TransactionTable
+          transactions={this.state.transactions}
+          newTransactionHandler={this.addTransaction} />
         </div>
     );
   }
