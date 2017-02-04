07d938c04c74b224d767aea0dd729fb4b7d22a88
Jason Petsod <jason@petsod.org>
2017-02-04 11:10:49 -0500

    Move TransactionRow and TransactionTable to a separate file

--- prototype2/frontend/loony.jsx
+++ prototype2/frontend/loony.jsx
@@ -1,69 +1,7 @@
-import accounting from 'accounting';
-import moment from 'moment';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import uuid from 'uuid';
 
-import AddTransactionRow from './addTransactionRow.jsx';
-import * as propTypes from './propTypes';
-
-class TransactionRow extends React.Component {
-  static propTypes = {
-    transaction: propTypes.transaction,
-  };
-
-  render() {
-    return (
-        <tr>
-          <td>{this.props.transaction.account}</td>
-          <td>{moment(this.props.transaction.dateMs).format('YYYY-MM-DD')}</td>
-          <td>{this.props.transaction.payee}</td>
-          <td>{this.props.transaction.category}</td>
-          <td>{this.props.transaction.memo}</td>
-          <td style={{textAlign:'right'}}>
-            {accounting.formatMoney(this.props.transaction.outflow, '$')}
-          </td>
-          <td style={{textAlign:'right'}}>
-            {accounting.formatMoney(this.props.transaction.inflow, '$')}
-          </td>
-        </tr>
-    );
-  }
-}
-
-class TransactionTable extends React.Component {
-  static propTypes = {
-    transactions: React.PropTypes.arrayOf(propTypes.transaction),
-    // TODO: Add function signature.
-    newTransactionHandler: React.PropTypes.func
-  };
-
-  render() {
-    const rows = this.props.transactions.map((t) =>
-        <TransactionRow key={t.id} transaction={t} />
-    );
-    return (
-        <table>
-        <thead>
-          <tr>
-            <th>Account</th>
-            <th>Date</th>
-            <th>Payee</th>
-            <th>Category</th>
-            <th>Memo</th>
-            <th>Outflow</th>
-            <th>Inflow</th>
-          </tr>
-        </thead>
-        <tbody>
-        {rows}
-        <AddTransactionRow
-          newTransactionHandler={this.props.newTransactionHandler} />
-        </tbody>
-        </table>
-    );
-  }
-}
+import TransactionTable from './transactionTable.jsx';
 
 class App extends React.Component {
   constructor(props) {
