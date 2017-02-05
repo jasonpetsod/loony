facc1857896c7e88541e0df459d304ad1f816d7d
Jason Petsod <jason@petsod.org>
2017-02-05 11:07:48 -0500

    Refactor submit handling tests
    Move setField to a new common describe() shared between the with
    initialTransactionData and without case.

--- prototype2/frontend/test/MutableTransactionRow.jsx
+++ prototype2/frontend/test/MutableTransactionRow.jsx
@@ -9,24 +9,8 @@ describe('<MutableTransactionRow />', function () {
   it('renders with initialTransactionData');
   it('renders without initialTransactionData');
 
-  it('handles submission', function () {
-    let receivedData = null;
-    const handler = (data) => {
-      receivedData = data;
-    };
-
-    const options = {
-      attachTo: global.document.createElement('tbody'),
-    };
-    const wrapper = mount(
-      <MutableTransactionRow
-        submitHandler={handler}
-        submitButtonLabel="Submit"
-      />,
-      options);
-
-    // Edit fields.
-    const setField = (name, value) => {
+  describe('handles submission', function () {
+    const setField = (wrapper, name, value) => {
       const field = wrapper.find({ name });
       const event = {
         target: {
@@ -37,33 +21,57 @@ describe('<MutableTransactionRow />', function () {
       field.simulate('change', event);
     };
 
-    const fields = {
-      date: '2017-01-01',
-      account: 'Cash',
-      payee: 'Mu Ramen',
-      category: 'Restaurants',
-      memo: 'yay noodles',
-      outflow: '23.00',
-      inflow: '19.89',
+    const setFields = (wrapper, fields) => {
+      Object.keys(fields).forEach((name) => {
+        setField(wrapper, name, fields[name]);
+      });
     };
-    Object.keys(fields).forEach((name) => { setField(name, fields[name]); });
 
-    // Click submit.
-    wrapper.find('input[type="submit"]').simulate('click');
+    it('without initialTransactionData', function () {
+      let receivedData = null;
+      const handler = (data) => {
+        receivedData = data;
+      };
 
-    // Make sure handler() is called with the right data.
-    const expectedData = {
-      id: '',
-      dateMs: 1483228800000,
-      account: 'Cash',
-      payee: 'Mu Ramen',
-      category: 'Restaurants',
-      memo: 'yay noodles',
-      outflow: 23,
-      inflow: 19.89,
-    };
-    assert.deepEqual(receivedData, expectedData);
+      const options = {
+        attachTo: global.document.createElement('tbody'),
+      };
+      const wrapper = mount(
+        <MutableTransactionRow
+          submitHandler={handler}
+          submitButtonLabel="Submit"
+        />,
+        options);
+
+      // Edit fields.
+      const fields = {
+        date: '2017-01-01',
+        account: 'Cash',
+        payee: 'Mu Ramen',
+        category: 'Restaurants',
+        memo: 'yay noodles',
+        outflow: '23.00',
+        inflow: '19.89',
+      };
+      setFields(wrapper, fields);
+
+      // Click submit.
+      wrapper.find('input[type="submit"]').simulate('click');
+
+      // Make sure handler() is called with the right data.
+      const expectedData = {
+        id: '',
+        dateMs: 1483228800000,
+        account: 'Cash',
+        payee: 'Mu Ramen',
+        category: 'Restaurants',
+        memo: 'yay noodles',
+        outflow: 23,
+        inflow: 19.89,
+      };
+      assert.deepEqual(receivedData, expectedData);
+    });
+
+    it('with initialTransactionData');
   });
-
-  it('handles submission with initialTransactionData');
 });
