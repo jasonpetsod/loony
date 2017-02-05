074b2af326717f4267596716c8316b98da0ba81f
Jason Petsod <jason@petsod.org>
2017-02-05 03:56:56 -0500

    Test that App#addTransaction adds a TransactionRow

--- prototype2/frontend/test/App.jsx
+++ prototype2/frontend/test/App.jsx
@@ -1,9 +1,11 @@
 import { assert } from 'chai';
-import { shallow } from 'enzyme';
+import { mount, shallow } from 'enzyme';
+import 'jsdom-global/register';
 import React from 'react';
 
 import App from '../src/App';
 import LoonyInternalError from '../src/LoonyInternalError';
+import TransactionRow from '../src/TransactionRow';
 
 describe('<App />', function () {
   describe('#addTransaction', function () {
@@ -52,7 +54,31 @@ describe('<App />', function () {
         ['a', 'b', 'c']);
     });
 
-    it('should add a new row to TransactionTable');
+    it('should add a new TransactionRow', function () {
+      const transactions = {};
+      const wrapper = mount(<App transactions={transactions} />);
+      const app = wrapper.instance();
+
+      const newTransaction = {
+        id: 'c',
+        dateMs: 1483938000000,  // 2017-01-09 00:00 UTC-05:00
+        account: 'Chase Sapphire Reserve',
+        payee: 'Ippudo',
+        category: 'Restaurants',
+        memo: '',
+        outflow: 27.31,
+        inflow: 0,
+      };
+
+      assert.lengthOf(wrapper.find(TransactionRow), 0);
+
+      app.addTransaction(newTransaction);
+
+      const rows = wrapper.find(TransactionRow);
+      assert.lengthOf(rows, 1);
+      const transaction = rows.at(0).prop('transaction');
+      assert.equal(transaction.id, 'c');
+    });
   });
 
   describe('#editTransaction', function () {
