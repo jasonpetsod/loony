0bf1e62a70e334c77523132bf2cc0a719fd93a8d
Jason Petsod <jason@petsod.org>
2017-02-04 23:23:01 -0500

    Add basic test that ensures App#editTransaction edits state

--- prototype2/frontend/test/App.jsx
+++ prototype2/frontend/test/App.jsx
@@ -56,4 +56,48 @@ describe('<App />', function () {
 
     it('should add a new row to TransactionTable');
   });
+
+  describe('#editTransaction', function () {
+    it('should edit the transaction in state', function () {
+      const transactions = {
+        a: {
+          id: 'a',
+          dateMs: 1483246800000,  // 2017-01-01 00:00 UTC-05:00
+          account: 'Checking',
+          payee: 'Google Inc.',
+          category: 'Income for January',
+          memo: '',
+          outflow: 0,
+          inflow: 100.00,
+        },
+      };
+      const wrapper = shallow(<App transactions={transactions} />);
+      const app = wrapper.instance();
+
+      const newTransaction = {
+        id: 'a',
+        dateMs: 1483938000000,  // 2017-01-09 00:00 UTC-05:00
+        account: 'Chase Sapphire Reserve',
+        payee: 'Ippudo',
+        category: 'Restaurants',
+        memo: '',
+        outflow: 27.31,
+        inflow: 0,
+      };
+
+      app.editTransaction('a', newTransaction);
+
+      // FIXME: Accessing app.state directly is racy since setState only
+      // enqueues a state change.
+
+      assert.equal(app.state.transactions.a, newTransaction);
+      assert.sameMembers(Object.keys(app.state.transactions), ['a']);
+    });
+
+    it('should fail when the id does not exist');
+
+    it('ensures id parameter matches id in transaction');
+
+    it('should edit the existing transaction in TransactionTable');
+  });
 });
