468416d765c633c41504798e563f72e34214df89
Jason Petsod <jason@petsod.org>
2017-02-04 23:07:31 -0500

    Add a test for App#addTransaction

--- prototype2/frontend/test/App.jsx
+++ prototype2/frontend/test/App.jsx
@@ -1,5 +1,55 @@
+import { assert } from 'chai';
+import { shallow } from 'enzyme';
+import React from 'react';
+
+import App from '../App';
+
 describe('<App />', function () {
   describe('#addTransaction', function () {
-    it('should add a new TransactionRow');
+    it('should add a new transaction to state', function () {
+      const transactions = {
+        a: {
+          id: 'a',
+          dateMs: 1483246800000,  // 2017-01-01 00:00 UTC-05:00
+          account: 'Checking',
+          payee: 'Google Inc.',
+          category: 'Income for January',
+          memo: '',
+          outflow: 0,
+          inflow: 100.00,
+        },
+        b: {
+          id: 'b',
+          dateMs: 1483678800000,  // 2017-01-06 00:00 UTC-05:00
+          account: 'Cash',
+          payee: 'Raku',
+          category: 'Restaurants',
+          memo: '',
+          outflow: 27.31,
+          inflow: 0,
+        },
+      };
+      const wrapper = shallow(<App transactions={transactions} />);
+      const app = wrapper.instance();
+
+      app.addTransaction({
+        id: 'c',
+        dateMs: 1483938000000,  // 2017-01-09 00:00 UTC-05:00
+        account: 'Chase Sapphire Reserve',
+        payee: 'Ippudo',
+        category: 'Restaurants',
+        memo: '',
+        outflow: 27.31,
+        inflow: 0,
+      });
+
+      assert.sameMembers(
+        // FIXME: Accessing app.state directly is racy since setState only
+        // enqueues a state change.
+        Object.keys(app.state.transactions),
+        ['a', 'b', 'c']);
+    });
+
+    it('should add a new row to TransactionTable');
   });
 });
