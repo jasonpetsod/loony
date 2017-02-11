fabbb287a81952c2e7739502af74d930650b94a7
Jason Petsod <jason@petsod.org>
2017-02-11 10:17:31 -0500

    Support storing Firebase config locally and in env variables

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,6 +1,17 @@
-const config = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
-};
+// Support both Firebase config stored locally in a file (e.g. on dev
+// workstations) and passed in using environment variables (e.g. in Travis
+// CI).
+
+let config = null;  // eslint-disable-line import/no-mutable-exports
+try {
+  /* eslint-disable global-require */
+  config = require('./localFirebaseConfig').default;
+  /* eslint-enable global-require */
+} catch (e) {
+  config = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+  };
+}
 
 export default config;
